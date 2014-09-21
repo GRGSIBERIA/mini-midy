84e3a6a2ff641cb32ff3525c7f51a88e926f8d0f
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-09-21 21:30:15 +0900

    hoge

--- Header.hpp
+++ Header.hpp
@@ -1,8 +1,10 @@
 #pragma once
 #include <string>
+#include <list>
 
 #include "FormatBase.hpp"
 #include "DeltaTime.hpp"
+#include "Track.hpp"
 
 
 namespace mmidy
@@ -14,15 +16,22 @@ namespace mmidy
 		*/
 		class Header : public FormatBase
 		{
-		public:
+		private:
 			std::string magic_code;
 			size_t data_size;
 			ushrot format;
 			ushrot number_of_tracks;
 			DeltaTime delta_time;
 
-		public:
+			std::list<Track> tracks;
 
+		private:
+			void CreateTracks(const ushrot number_of_tracks)
+			{
+				tracks = std::list<Track>();
+			}
+
+		public:
 		};
 	}
 }
